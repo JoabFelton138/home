a2ebd25a3e5634d8096cb32cd59122d79899fa85
Joab <k1730992@kingston.ac.uk>
2021-01-11 00:15:59 +0000

    Updates

---
+++ precache-manifest.aa6377b2b53b9b07076013136cdfc2f8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92907af0fc8e17555858264e516a356c",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "3c0d3684637b5fd25471",
+    "url": "/home/static/css/main.956fad23.chunk.css"
+  },
+  {
+    "revision": "291f471688b0108d96fa",
+    "url": "/home/static/js/2.33ef47b7.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/home/static/js/2.33ef47b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c0d3684637b5fd25471",
+    "url": "/home/static/js/main.dc1f42c2.chunk.js"
+  },
+  {
+    "revision": "c0578b487099793a068c",
+    "url": "/home/static/js/runtime-main.3c590061.js"
+  },
+  {
+    "revision": "7c23718c1f487d90cf42e0e8db4bb82e",
+    "url": "/home/static/media/0.7c23718c.jpg"
+  },
+  {
+    "revision": "60100fd7700b166318d6bf361804d7be",
+    "url": "/home/static/media/resume.60100fd7.pdf"
+  }
+]);
